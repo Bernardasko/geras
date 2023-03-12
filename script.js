84b872a45520ae90e5e71e1b0e480d21d23e948c
Bernardasko <115994039+Bernardasko@users.noreply.github.com>
2023-03-12 21:06:27 +0200

    master

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function myFunction() {
+function darbuotojas1() {
     var x = document.getElementById("bars");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
-    }
-       
+    }    
+}
+function darbuotojas2() {
+    var x = document.getElementById("bars1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }    
+}
+function darbuotojas3() {
+    var x = document.getElementById("bars2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }    
 }
